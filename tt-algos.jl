04e66230b78b2cdbd65fbf6240d5f92d10c52788
anthonykyoon <138504553+anthonykyoon@users.noreply.github.com>
2025-01-03 16:31:24 -0800

    added docstrings

--- tt-algos.jl
+++ tt-algos.jl
@@ -1,10 +1,20 @@
 using LinearAlgebra
-using TSVD
 using OffsetArrays
 
+#I didn't know how to deal with the data types of tensor trains, so I used the Any datatype
 
 #helper functions
 function mode_k_contraction(base, added, k::Int64)
+    """
+    Performs the mode k contraction
+
+    Arguments:
+        base: The tensor whose kth index will be replaced
+        added: The sacrifical tensor who will be added into the tensor 
+        k: The mode in which this will be applied 
+    Returns:
+        Tensor with the contraction completed 
+    """
     #performing the actual contraction itself
     base_perm = permutedims(base, [setdiff(1:ndims(base), [k])...])
     base_reshaped = reshape(base_perm, size(base)[k], : )
@@ -19,6 +29,13 @@ end
 function tt_contraction(tt_train::Any)
     """
     Make the tensor train back into a tensor. 
+
+    Arguments:
+        tt_train: Tensor Train 
+
+    Returns: 
+        first: Tensor derived from the tensor train 
+
     """
     @assert length(size(tt_train[1])) == 3
     d = length(tt_train)
@@ -29,7 +46,6 @@ function tt_contraction(tt_train::Any)
     middle_indices[1] = dimensions_1[2]
     first = reshape(tt_train[1],dimensions_1[1]* dimensions_1[2], dimensions_1[3])
     for iteration in 2:d
-        print("iteration for the contraction is equal to $iteration \n" )
         interest_tensor_dims = size(tt_train[iteration])
         middle_indices[iteration] = interest_tensor_dims[2]
         interest_tensor = reshape(tt_train[iteration], interest_tensor_dims[1], interest_tensor_dims[2] * interest_tensor_dims[3])
@@ -49,18 +65,33 @@ function tt_contraction(tt_train::Any)
 end
 
 function frobenius_tt_1(tt_train::Any)
+    """
+    Returns the frobenius norm of a tensor train
+    Arguments:
+        tt_train: A vector holding the tensor trains
+    Returns:
+        Frobenis Norm as a Float value
+    """
     return sqrt(sum(abs2, tt_contraction(tt_train)))
 end
 
 function frobenius_tt(tt::Any) 
+    """
+    Alternative way of doing the frobenius norm
+
+    Arguments: 
+        tt: Tensor train 
+
+    Returns:
+        Frobenius norm as a Float
+    """
     d = length(tt)
     # F is our accumulator for the partial inner product
     # Start with a 1×1 array with value 1.0
     F = [1.0]
 
-    # Loop over the d cores
     for k in 1:d
-        core = tt[k]  # shape = (r_{k-1}, n_k, r_k)
+        core = tt[k]  
         (rL, n, rR) = size(core)
         
         F_new = zeros(rR, rR)
@@ -75,11 +106,21 @@ function frobenius_tt(tt::Any)
         F = F_new
     end
     
-    return sqrt(F[1, 1])  # Frobenius norm
+    return sqrt(F[1, 1])  
 end
 
 
 function rq(A::Any)
+    """
+    Performs a QR decomposition on the rows
+
+    Arguments:
+        A: Matrix to be decomposed
+
+    Returns: 
+        Q = Transpose of the Q matrix such that the rows are orthonormal
+        R = Transpose of the R matric such that R is upper triangular
+    """
     F = qr(Transpose(A))
     R = Matrix(F.R)
    Q = Matrix(F.Q)
@@ -88,6 +129,16 @@ end
 
 
 function padding_tensor(tensor::Any, target_index::Int128)
+    """
+    Pads a tensor for the use in the tt_direct_sum function
+
+    Arguments:
+        tensor: The tensor to be padded
+        target_index: The axis in which we will pad in 
+    
+    Returns:
+        padded_tensor: The padded tensor
+    """
     rank_l, n, rank_r = size(tensor)
     
     if n == target_index || n > target_index 
@@ -102,7 +153,17 @@ end
 
 #actual functions
 function TT_Direct_Sum(tt_a, tt_b)
-    #Dimension of tt_a and TT)2
+    """
+    Takes two tensor trains and takes the direct sum of the two
+
+    Arguments: 
+        tt_a: first tensor train 
+        tt_b: second tensor train 
+
+    Returns: 
+        tt_train: The two tensor trains summed together.
+    """
+    #Dimension of tt_a and tt_b
     d_a = length(tt_a)
     d_b = length(tt_b)
     
@@ -178,6 +239,16 @@ function TT_Direct_Sum(tt_a, tt_b)
 end 
 
 function TT_SVD_1(input_tensor::Any, error)
+    """
+    Takes a tensor, and decomposes it into a tensor train of length d, where d is the rank of teh Tensor
+
+    Arguments:
+        input_tensor: the tensor to be decomposed into a tensor train 
+        error: the maximum tolerated error
+
+    Returns:
+        tt_train: the resultant tensor train
+    """
     #rank of the tensor
     d = ndims(input_tensor)
     #storing the tensor train 
@@ -206,30 +277,7 @@ function TT_SVD_1(input_tensor::Any, error)
                 
                 next_index = cld(prod(size(W)), (r[i-1] * n[i]))
                 amount_to_be_padded = (next_index * r[i-1] * n[i]) - prod(size(W))
-
-                println("my math was wrong and I have no idea how to pad this properly")
-                throw("Help please")
-                # if mod(amount_to_be_padded, (r[i-1] * r[i])) == 0
-                #     added_index = div(amount_to_be_padded, (r[i-1] * r[i]))
-                #     padding = zeros(Int(r[i-1]), Int(added_index), r[i])
-                #     W = cat(W, padding; dims=2)
-                #      remaining_index = Int(prod(size(W)) / (r[i-1] * n[i]))
-                #     println("Returning index from padding strategy 1.")
-                # elseif mod(amount_to_be_padded, r[i] * n[i]) == 0
-                #     added_index = div(amount_to_be_padded, (r[i] * n[i]))
-                #     padding = zeros(added_index, n[i], r[i])
-                #     W = cat(W, padding; dims=1)
-                #      remaining_index = Int(prod(size(W)) / (r[i-1] * n[i]))
-                #     println("Returning index from padding strategy 2.")
-                # else
-                #     println("Padding strategy failed.")
-                #     println("iteration = $i")
-                #     println(r[i-1])
-                #     println(r[i])
-                #     println(n[i])
-                #     println(amount_to_be_padded)
-                #     throw("Unable to resolve padding requirements.")
-                # end
+                throw("Something went wrong with the indices")
             else
                 println("A severe error has occurred: $bad_case")
                 throw(bad_case)
@@ -252,9 +300,9 @@ function TT_SVD_1(input_tensor::Any, error)
         cutoff = findlast(cumsum_singular.>= (singular_squared - trunc_param^2))
         
 
-        # if cutoff === nothing
-        #     cutoff = 1
-        # end 
+        if cutoff === nothing
+            cutoff = 1
+        end 
 
         U_trunc = U[:, cutoff:end]
         S_trunc = diagm(S[cutoff:end])
@@ -265,60 +313,10 @@ function TT_SVD_1(input_tensor::Any, error)
         W = S_trunc * Transpose(V_trunc)
     end
     
-    tt_train[d] = reshape(W, Int(r[d]), n[d], 1)
+    tt_train[d] = reshape(W, Int(r[d-1]), n[d], 1)
     return tt_train 
 end
 
-# function TT_Round(input_tt:: Any, error_threshold::Float64)
-#     # #Store the tensor cores 
-#     # TT_cores = Vector{Any}(undef, d)
-#     #How long TT is 
-#     d = length(input_tt)
-#     #Copying the input_tt over
-#     G = copy(input_tt)
-#     #Rank of Each Core 
-#     #TODO fix the frobenius norm 
-#     tt_norm = sqrt(sum(abs2, tt_contraction(G)))
-#     #Truncation Parameter 
-#     trunc_param = (error_threshold) / (sqrt(d - 1)) * tt_norm
-
-
-#     #Q and R can be computed by the rehspaingof the tensor G_k by reshaping it into r_{k-1} times n_k r_k
-#     for i in d:2:(-1)
-#         dim_current_core = size(G[i])
-#         Q, R = qr(reshape(G[i], Int(dim_current_core[1]), Int(dim_current_core[2] * dim_current_core[3])))
-#         G[i] = Q
-#         #need to reshape this tensor 
-#         new_dim_core = size(G[i])
-#         G[i] = reshape(G[i], new_dim_core[1], new_dim_core[2], new_dim_core[3])
-#         G[i-1] = mode_k_contraction(G[i-1], R, 3)
-#     end
-
-#     for i in 1:(d-1)
-#         println("iteration $i")
-#         dim_current_core = size(G[i])
-#         reshaped_tensor = reshape(G[i], Int(dim_current_core[1]), Int(dim_current_core[2] * dim_current_core[3]))
-#         U, S, V = svd(reshaped_tensor)
-
-#         #truncation step 
-#         println(S)
-#         cumsum_singular = cumsum(S.^2)
-#         singular_squared = sum(S.^2)
-#         println(cumsum_singular)
-#         println(singular_squared)
-#         cutoff = findlast(cumsum_singular.<= (singular_squared - trunc_param))
-
-#         if cutoff === nothing 
-#             throw("cutoff does not exists")
-#         end
-
-#         G[i] = U[:, 1:cutoff]
-#         S = Diagonal(S[1:cutoff])
-#         V_Trunc = V[1:cutoff, :]
-#         G[i+1] = S * V_Trunc
-#     end
-#     return G
-# end 
 
 function TT_Round_1(tt_train, error::Float64, r_max:: Int64)
     """
@@ -340,16 +338,6 @@ function TT_Round_1(tt_train, error::Float64, r_max:: Int64)
         Q, R = rq(folding_matrix)
         middle_index = size(Q, 1)
         B[k] = reshape(Transpose(Q), middle_index, nk, rk)
-
-        # F  = qr(Transpose(folding_matrix))
-
-        # Q = Matrix(F.Q)
-        # R = F.R
-        # #index caused by the qr decomp 
-        # middle_index = size(Q, 2)
-
-        # B[k] = reshape(Transpose(Q), middle_index, nk, rk)
-
         #contracting R into the matrix to the right of it 
         Gprev = B[k-1]
         p_rk_1, p_nk, p_rk = size(Gprev)
@@ -375,15 +363,12 @@ function TT_Round_1(tt_train, error::Float64, r_max:: Int64)
         S = reverse(S)
         cumsum_singular = cumsum(S.^2)
         singular_squared = sum(S.^2)
-        println("Iteration $k")
-        println(S)
         cutoff = findlast(cumsum_singular.>= (singular_squared - trunc_param^2))
         println(S[cutoff:end])
         if cutoff === nothing
             println("reassigment of cutoff to length of 1")
             cutoff = 1
         end 
-        println("cutoff = $cutoff")
 
         Utrunc = U[:, cutoff:end]
         Strunc = diagm(S[cutoff:end])
