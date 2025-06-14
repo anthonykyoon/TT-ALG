386a5036ce09f2e6c9dedf44b3487f2228758d0c
anthonykyoon <138504553+anthonykyoon@users.noreply.github.com>
2025-06-14 02:02:01 -0500

    I dont think im doing this right

--- cros_practice.jl
+++ cros_practice.jl
@@ -9,39 +9,53 @@ function cabs(x,y)
     return y 
 end
 
-function formrot(a,b)
-    if abs(a) >= abs(b) 
+function formrot(a, b)
+    if abs(a) >= abs(b)
         r = b / a 
         factor = sqrt(1 + r^2)
-        a = abs(a) * factor
-        c = factor^(-1)
+        a_new = abs(a) * factor
+        c = inv(factor)
         s = r * c
     else
         r = a / b 
         factor = sqrt(1 + r^2)
-        b = abs(b) * factor 
-        s = factor^(-1)
+        a_new = abs(b) * factor 
+        s = inv(factor)
         c = r * s
     end
-    return [[a,b], [c,s], [c , -s ; s, c]]
+    G = [c -s; s c]  # The rotation matrix
+    return a_new, c, s, G
 end
 
-function applyrot(x,y,c,s,n:: Int)
+
+function applyrot(x, y, c, s, n::Int)
     @assert length(x) >= n && length(y) >= n
-    temp = c * x[1:n] + s * y(1:n)
-    y[1:n] = -s * x[1:n] + c * y(1:n)
-    x[1:n] = temp
-    return [x, y]
+    temp = copy(c * x[1:n] + s * y[1:n])
+    y[1:n] .= -s * x[1:n] .+ c * y[1:n]
+    x[1:n] .= temp
+    return nothing
 end
+
 
 function forchase(gamma, phi, z, n)
-    cn, sn = formrot(z[n-1], z[n])[2]
+    _, cn, sn, _ = formrot(z[n-1], z[n])
     e = -sn * gamma[n-1]
     gamma[n-1] = cn * gamma[n - 1]
     phi[n-1] = sn * gamma[n]
     gamma[n] = cn * gamma[n]
     cn, sn = formrot(gamma[n], e)[2]
-    result_applyrot = applyrot(phi[n-1], gamma[n-1], cn, sn, 1)
+    applyrot(phi[n-1], gamma[n-1], cn, sn, 1)
+    for i in reverse((n-2):1)
+        _, cn, sn, _ = formrot(z[i], z[i+1])
+        e = -sn * gamma[i]
+        gamma[i] = cn * gamma[i]
+        phi[i] = sn * gamma[i+1]
+        gamma[i+1] = cn * gamma[i+1]
+        d = cn * phi[i+1]
+        phi[i+1] = sn * phi[i+1]
+        _, cn, sn, _ =formrot(gamma[i], b)
+        applyrot()
+    end
 end
 
 
